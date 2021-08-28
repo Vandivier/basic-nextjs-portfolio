3aaf6ad5769496d073d438f8c0c84fb3315d74fa
Vandivier <john@afterecon.com>
2021-08-28 17:18:45 -0400

    feat: initial merge ts example
    to gh-pages example

--- basic-nextjs-portfolio/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-module.exports = {
-  assetPrefix: "/basic-nextjs-portfolio",
-  basePath: "/basic-nextjs-portfolio",
-  images: { loader: "imgix" },
-  reactStrictMode: true,
-};
